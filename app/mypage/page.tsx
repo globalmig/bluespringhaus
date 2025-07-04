501d1f63bd53406cc8e869b4ff46788b9bc13acc
연구개발전담부 <wdpb@mig.kr>
2025-07-04 17:26:14 +0900

    fix: 더미데이터 삭제 추후 db연결 예정

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -6,66 +6,6 @@ import { useAuth } from "../contexts/AuthContext";
 import { useRouter } from "next/navigation";
 
 // TODO: 추후 API 연결
-const mockData = [
-  {
-    id: 1,
-    name: "김은하",
-    desc: "IT 스타트업 창업가 / UX 전문가",
-    tag: ["리더십", "I윤리"],
-    image: "/images/speaker01.jpg",
-  },
-  {
-    id: 2,
-    name: "박지훈",
-    desc: "청소년 심리상담사 / 강연 200회 이상",
-    tag: ["리더십", "AI윤리"],
-    image: "/images/speaker02.jpg",
-  },
-  {
-    id: 3,
-    name: "이유정",
-    desc: "전직 아나운서 / 커뮤니케이션 코치",
-    tag: ["리더십", "AI윤리"],
-    image: "/images/speaker03.jpg",
-  },
-  {
-    id: 4,
-    name: "최민수",
-    desc: "전직 프로게이머 / 게임 리더십 전문가",
-    tag: ["리더십", "AI윤리"],
-    image: "/images/speaker04.jpg",
-  },
-  {
-    id: 5,
-    name: "홍다은",
-    desc: "비영리단체 활동가 / 환경 교육 강사",
-    tag: ["지속가능성", "AI윤리"],
-    image: "/images/speaker05.jpg",
-  },
-  {
-    id: 6,
-    name: "정재형",
-    desc: "인공지능 연구원 / AI 윤리 강연자",
-    tag: ["해외취업", "AI윤리"],
-    image: "/images/speaker06.jpg",
-  },
-  {
-    id: 7,
-    name: "서윤지",
-    desc: "디지털 노마드 / 글로벌 커리어 강연가",
-    tag: ["해외취업", "AI윤리", "리더십"],
-    image: "/images/speaker07.jpg",
-  },
-  {
-    id: 8,
-    name: "안도현",
-    desc: "작가 / 시인 / 창작 워크숍 진행",
-    tag: ["해외취업", "글쓰기"],
-    image: "/images/speaker08.jpg",
-  },
-];
-
-
 
 const progressText = [
   { icon: "", title: "시작", sub: "문의완료" },
@@ -117,15 +57,15 @@ export default function mypage() {
         </div>
       </section>
 
-      {/* 문의 답변 섹션 */}
+      {/* 문의 답변 섹션
       <section className="bg-white rounded-lg p-10">
         <CardItem slides={mockData} />
-      </section>
+      </section> */}
 
-      {/* 문의 신청한 리스트 섹션 */}
+      {/* 문의 신청한 리스트 섹션
       <section className="bg-white rounded-lg p-10">
         <CardItem slides={mockData} />
-      </section>
+      </section> */}
 
       {/* 문의 답변 섹션  TODO: 후순위*/}
       {/* <CardList slides={[]} title={"이전 문의 내역"} sub={"이전에 문의하신 연사분들을 확인해보세요!"} /> */}
