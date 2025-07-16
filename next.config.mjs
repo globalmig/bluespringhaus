78ff9669f63feac10e40bac2a590fcc5730006d2
연구개발전담부 <wdpb@mig.kr>
2025-07-16 14:47:39 +0900

    fix: 이미지 허락 도메인 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["bluespringhaus-rbt5.vercel.app/"],
+  },
+};
 
 export default nextConfig;
