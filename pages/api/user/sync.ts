17d07358673e5a20b5302cb022cab0bffa2a3b44
연구개발전담부 <wdpb@mig.kr>
2025-10-02 16:06:05 +0900

    fix

--- pages/api/user/sync.ts
+++ pages/api/user/sync.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth/next";
 
 import { createClient } from "@supabase/supabase-js";
-import { authOptions } from "../auth/[...nextauth]";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
